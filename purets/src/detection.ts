51ce4cd59c031e8f2aa64fa7a5e58293aa0b188a
Kundan10424 <kundankumar10424>
2026-01-03 17:01:11 +0530

    Detection & Type Narrowing

--- purets/src/detection.ts
+++ purets/src/detection.ts
@@ -48,3 +48,75 @@ function isAdminAccount(account: User | Admin){
     }
     return false;
 }
+
+function logValue(x: Date | string){
+    if(x instanceof Date){
+        console.log(x.toUTCString());
+    } else{
+        console.log(x.toUpperCase());
+    }
+}
+
+type Fish = {swim:() => void}
+type Bird = {fly: () => void}
+
+function isFish(pet: Fish | Bird): pet is Fish{
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet: Fish | Bird){
+    if(isFish(pet)){
+        pet
+        return "fish food";
+    } else{
+        pet
+        return "bird food";
+    }
+}
+
+// discriminated Unions
+
+interface Circle{
+    kind: "circle",
+    radius: number
+}
+
+interface Square{
+    kind: "square",
+    side: number
+}
+
+interface Rectangle{
+    kind: "rectangle",
+    length: number,
+    width: number
+}
+
+type Shape = Circle | Square | Rectangle
+
+function getTrueShape(shape: Shape){
+    if(shape.kind === "circle"){
+        return Math.PI * shape.radius ** 2;
+    } else if(shape.kind === "rectangle"){
+        return shape.length * shape.width;
+    }
+    return shape.side * shape.side;
+}
+
+function getArea(shape: Shape){
+    switch(shape.kind){
+        case "circle":
+            return Math.PI*shape.radius**2
+        
+            case "square":
+            return shape.side*shape.side
+        
+        case "rectangle":
+            return shape.length*shape.width
+        default:
+            const _defaultShape: never = shape
+            return _defaultShape
+    }
+}
+
+export {}
